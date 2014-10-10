5ef0dea7f0dfefd0380a47aa1dd8803ffd9832d2
kterusaki <kterusaki@gmail.com>
2014-10-10 15:18:55 -0700

    added posts#destroy

--- terusaki_kevin/reddit_clone/app/controllers/posts_controller.rb
+++ terusaki_kevin/reddit_clone/app/controllers/posts_controller.rb
@@ -28,11 +28,18 @@ class PostsController < ApplicationController
 	def update
 		@post = Post.find(params[:id])
 
-		if @post.update
+		if @post.update(post_params)
 			redirect_to @post
 		else
 			rerender 'update'
 		end
+	end
+
+	def destroy
+		@post = Post.find(params[:id])
+		@post.destroy
+
+		redirect_to posts_path
 	end
 
 	private 
